4e66e23bf87c2e4f9a89c86b3c2c53a0f9f41cd3
rafaelaxcelere <rafael@axcelere.com>
2023-07-11 19:59:44 -0300

    [MIGRATION][account_ux] account_payment_method: Script to change values to avoid duplicated pk in databse

--- account_ux/migrations/16.0.1.0.0/pre-migration.py
+++ account_ux/migrations/16.0.1.0.0/pre-migration.py
@@ -1,13 +1,22 @@
 from openupgradelib import openupgrade
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
     """
     """
+    _logger.info('UPGRADE 15->16 AXCELERE')
+    env.cr.execute("""
+        UPDATE account_payment_method SET code = 'new_third_party_checks_origin' WHERE code = 'new_third_party_checks' AND payment_type = 'inbound';
+    """)
     view = env.ref('account_ux.view_move_line_tree', raise_if_not_found=False)
     if view:
         view.unlink()
     view = env.ref('account_ux.view_move_line_tree_grouped', raise_if_not_found=False)
     if view:
         view.unlink()
+
+
