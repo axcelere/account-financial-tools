7e643b40d6c2fb1143be4e404ad179d133ff8d45
Rafael Aguero Baquero <raguero@quanam.com>
2025-04-19 15:08:49 -0300

    [MIG] account_ux

--- account_ux/migrations/18.0.1.0.0/pre-migration.py
+++ account_ux/migrations/18.0.1.0.0/pre-migration.py
@@ -10,3 +10,6 @@ def migrate(cr, version):
     view = env.ref("account_ux.view_account_form", raise_if_not_found=False)
     if view:
         view.unlink()
+    view = env.ref("account_ux.view_account_change_currency", raise_if_not_found=False)
+    if view:
+        view.unlink()
